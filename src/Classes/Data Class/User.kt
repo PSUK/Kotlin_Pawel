f9d45a4206811ed36851cc4b027e03f38ac8b4fa
Pawel <bitnet@o2.pl>
2021-10-03 22:41:14 +0100

    Data class

--- src/Classes/Data Class/User.kt
+++ src/Classes/Data Class/User.kt
@@ -1,1 +1,1 @@
-data class User(val name: String, val surename: String, var telNumberFormatException: java.lang.NumberFormatException)
+data class User(val name: String, val surename: String, var telNumberFormatException: Int)
